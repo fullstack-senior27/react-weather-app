b808016117a42f0d80c83d2c511a4eaf4e1ea95f
fullstack-senior27 <yt000605@gmail.com>
2024-02-03 05:58:26 +0900

    wip(current-weather): current-weatherコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Header } from "./components/Header/Header";
 import './App.scss';
+import { CurrentWeather } from "./components/CurrentWeather/CurrentWeather";
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
+      <CurrentWeather></CurrentWeather>
     </div>
   );
 }
